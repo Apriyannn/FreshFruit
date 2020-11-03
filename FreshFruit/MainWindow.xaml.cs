b14279cd7c50deef2a50d563339226053d2da178
Apryan <Apriyansptr@gmail.com>
2020-11-03 22:31:44 +0700

    Add project files.

---
+++ FreshFruit/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using FreshFruit.Controller;
+using FreshFruit.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FreshFruit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, BucketEventListener
+    {
+        Seller Apriyan;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Bucket keranjangBuah = new Bucket(2);
+            BucketController bucketController = new BucketController(keranjangBuah, this);
+
+            Apriyan = new Seller("toni", bucketController);
+
+            ListBoxBucket.ItemsSource = keranjangBuah.findAll();
+        }
+
+        public void onFailed(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void onSucceed(string message)
+        {
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAnggurClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit anggur = new Fruit("Anggur");
+            Apriyan.addFruit(anggur);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddAppleClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Apple = new Fruit("Apple");
+            Apriyan.addFruit(Apple);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddBananaClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit Banana = new Fruit("Banana");
+            Apriyan.addFruit(Banana);
+
+            ListBoxBucket.Items.Refresh();
+        }
+
+        private void OnButtonAddOrangeClicked(object sender, RoutedEventArgs e)
+        {
+            Fruit orange = new Fruit("orange");
+            Apriyan.addFruit(orange);
+
+            ListBoxBucket.Items.Refresh();
+        }
+    }
+}
